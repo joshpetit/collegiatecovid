4a516934508edc0e7121c482ec76013cf5d12584
joshu <joshuapetitma@yahoo.com>
2020-12-05 17:43:17 -0500

    creates class structures

--- backend/bot/model/college.py
+++ backend/bot/model/college.py
@@ -3,7 +3,19 @@ from bs4 import BeautifulSoup
 
 class College:
 
-    def __init__(self, name: str, site: str, location: str) :
+    def __init__(self, name: str, site: str, location: str):
         self.name = name;
         self.site = site;
         self.location = location;
+
+    def querySite(self):
+        pass
+
+
+class Duke(College):
+    def __init__(self, name, site, location):
+        Person.__init__(self, name, site, location)
+
+    def querySite(self):
+        pass
+

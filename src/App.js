94e6de2a9f6970a7b2f1f0a329f3b9e48696d36b
awandke <awandke@stu.naperville203.org>
2020-12-05 17:33:08 -0600

    Created overall outline for project.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

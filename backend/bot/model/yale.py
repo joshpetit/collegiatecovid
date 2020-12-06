92f26271a090770d04a4c60b189a9802cf542aa3
joshu <joshuapetitma@yahoo.com>
2020-12-06 10:34:44 -0500

    retrieve pos cases from Yale

---
+++ backend/bot/model/yale.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+from .college import College
+
+
+class Yale(College):
+    def __init__(self):
+        College.__init__(self, 'Yale', 'https://covid19.yale.edu/yale-statistics/yale-covid-19-statistics-data-tables#yale-positive-cases-data')
+
+    def query_site(self):
+        res = {}
+        pos_cases = self.get_pos_cases()
+        res['pos_cases'] = pos_cases
+        return res
+
+    def get_pos_cases(self):
+        table = self.page.select('tbody tr td')
+        pos_cases = table[0].text
+        return int(pos_cases.replace(',',''))
+
+
+college = Yale()
+print(college.query_site())
+

d05e7bc164ae2bd55cc5c9aaf095424b02d94947
awandke <awandke@stu.naperville203.org>
2020-12-05 20:17:54 -0600

    Added some basic functionality to some of the React components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Header from "./SchoolPageComponents/Header";
 
 function App() {
   return (
---
+++ src/SchoolPageComponents/PolicySection.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function PolicySection() {
+
+    return (
+        <div>
+            <ul>
+                
+            </ul>
+        </div>
+    )
+}

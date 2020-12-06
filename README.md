6e557123589c851126fc11b60be2acb9afde7270
awandke <awandke@stu.naperville203.org>
2020-12-06 13:50:20 -0600

    Added connectivity to database

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 ## Available Scripts
 
 In the project directory, you can run:
-
 ### `npm start`
 
 Runs the app in the development mode.\
--- src/data/schoolData.js
+++
@@ -1,9 +0,0 @@
-const schoolData = [
-    {
-        "name": "Duke University",
-        "cases": 12000000,
-        "tests": 23,
-        "positivity": 6.23,
-        "isolation": 28
-    }
-];

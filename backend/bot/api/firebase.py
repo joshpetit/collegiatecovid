9f2286f64c94b4755c2445834fa76ff55c9d5ae5
joshu <joshuapetitma@yahoo.com>
2020-12-06 10:13:16 -0500

    pushes Duke to firestore

--- backend/bot/api/firebase.py
+++ backend/bot/api/firebase.py
@@ -5,3 +5,14 @@ cred = credentials.Certificate('./collegiatecovid.json')
 app = initialize_app(cred)
 db = firestore.client()
 
+
+doc_ref = db.collection('colleges_stats').document('Duke University')
+
+query = Duke().query_site()
+doc_ref.set({
+    'isolation': query['isolation'],
+    'pos_cases': query['pos_cases'],
+    'total_tests': query['total_tests'],
+    'pos_rate': query['pos_rate']
+})
+

c0d95090f42153404ac92f21dd4e8e589552636c
Herbert Jiafu Wang <herbert.wang@duke.edu>
2020-12-06 12:02:29 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
 import React from "react";
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import SearchBarz from "./HomePageComponents/SearchBarz"
-import HomePage from "./HomePage.js"
-import SchoolPage from "./SchoolPageComponents/SchoolPage"
 import NavBar from "./HomePageComponents/NavBar";
 
 export default function App() {

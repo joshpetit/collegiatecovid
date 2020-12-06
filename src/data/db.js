d78f26663071d52d2f1d9df7c4a5d301da63c2d2
joshu <joshuapetitma@yahoo.com>
2020-12-06 13:36:42 -0500

    creates function to get college names

--- src/data/db.js
+++ src/data/db.js
@@ -1,4 +1,21 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-import firebaseConfig from './secret.js'
+import {firebaseConfig} from './secret.js';
 
+firebase.initializeApp(firebaseConfig)
+let items = [];
+const ref = firebase.firestore().collection('colleges')
+
+export function getCollegesNames() {
+  let names = []
+  ref.onSnapshot( (q) => {
+      q.forEach( (doc) => {
+        doc.data()['Colleges'].forEach( (x) => {
+          names.push(x['name'])
+        })
+    });
+  })
+  return names;
+}
+
+console.log(getCollegesNames())

422bfb3d8dcaf683989d7ec76842684805d619e2
joshu <joshuapetitma@yahoo.com>
2020-12-06 06:09:36 -0500

    adds UNL

--- backend/bot/model/college.py
+++ backend/bot/model/college.py
@@ -81,11 +81,9 @@ class UIUC(College):
         self.page = page
         # await page.screenshot({'path': 'example.png'})
         content = await page.evaluate('document.body.textContent', force_expr=True)
-        print(content)
         testsData = re.findall("\\d*,\\d*", content)[1]
         testNum = int(testsData.replace(',',''))
         self.testNum = testNum
-        return testsNum
         await browser.close()
 
     def query_site(self):
@@ -150,7 +148,7 @@ class CMU(College):
     async def go(self):
         browser = await launch()
         page = await browser.newPage()
-        await page.goto(self.site, waitUntil='networkidle0')
+        await page.goto(self.site, waituntil='networkidle0')
         self.page = page
         element = await page.querySelector('.total-on-campus')
         toc = await page.evaluate('(e) => e.textContent', element)
@@ -171,6 +169,46 @@ class CMU(College):
     def get_number_tests(self):
         pass
 
-college = CMU()
+class UNL(College):
+    def __init__(self):
+        College.__init__(self, 'University of Nebraska Lincoln',
+                         'https://covid19.unl.edu/unl-covid-19-dashboard',
+                         'Lincoln, Nebraska')
+
+    async def go(self):
+        browser = await launch()
+        page = await browser.newPage()
+        await page.goto(self.site)
+        self.page = page
+        element = await page.querySelector('#total-daily-test-total')
+        total_str = await page.evaluate('(e) => e.textContent', element)
+        total_tests = int(total_str.replace(',',''))
+        self.total_tests = total_tests
+
+        element = await page.querySelector('#recent-week-positivity-rate')
+        pos_rate = await page.evaluate('(e) => e.textContent', element)
+        pos_rate = float(pos_rate)
+        self.pos_rate = pos_rate
+
+        element = await page.querySelector('#total-daily-positive-tests')
+        pos_cases = await page.evaluate('(e) => e.textContent', element)
+        pos_cases = int(pos_cases.replace(',',''))
+        self.pos_cases = pos_cases
+        await browser.close()
+
+    def query_site(self):
+        return {
+            'pos_cases': self.pos_cases,
+            'total_tests': self.total_tests,
+            'pos_rate': self.pos_rate
+        }
+
+    def get_page(self, site: str):
+        asyncio.get_event_loop().run_until_complete(self.go())
+
+    def get_number_tests(self):
+        pass
+
+college = UNL()
 res = college.query_site()
 print(res)

50c24348e4d573a2b03d68f55cde923e81203de1
joshu <joshuapetitma@yahoo.com>
2020-12-06 11:43:12 -0500

    gets positivity rate for gvsu

---
+++ backend/bot/model/gvsu.py
@@ -0,0 +1,38 @@
+import requests
+from .college import College
+from bs4 import BeautifulSoup
+
+class GVSU(College):
+    def __init__(self):
+        College.__init__(self, 'Grand Valley State University',
+                         'https://www.gvsu.edu/lakerstogether/data-dashboard-37.htm')
+
+
+    def query_site(self):
+        res = {}
+
+        res['total_tests'] = self.get_number_tests()
+        res['pos_cases'] = self.get_pos_cases()
+        res['pos_rate'] = self.getPosRate()
+        res['isolation'] = self.get_iso()
+        return res
+
+    def get_pos_cases(self):
+        pass
+
+    def get_number_tests(self):
+        tab = self.page.find('table', id='list_075C4C8D-A3EB-1735-D5AC3CC091608E84')
+        data = tab.findAll('td')
+        print(data)
+
+    def getPosRate(self):
+        tab = self.page.find('table', id='list_075C4C8D-A3EB-1735-D5AC3CC091608E84')
+        data = tab.findAll('td')[-1]
+        return float(data.text)
+
+    def get_iso(self):
+        pass
+
+college = GVSU()
+print(college.query_site())
+

2184f9d40a0ebf40347c5a5bdb962f6eac57d00c
joshu <joshuapetitma@yahoo.com>
2020-12-05 22:03:12 -0500

    adds Harvard

--- backend/bot/model/college.py
+++ backend/bot/model/college.py
@@ -11,20 +11,20 @@ class College:
         self.name = name;
         self.site = site;
         self.location = location;
-        self.page = self.getPage(self.site)
+        self.page = self.get_page(self.site)
 
-    def querySite(self):
+    def query_site(self):
         pass
 
-    def getPage(self, site: str):
+    def get_page(self, site: str):
         request = requests.get(site)
         soup = BeautifulSoup(request.content, 'html.parser')
         return soup;
 
-    def getNumberTests(self):
+    def get_number_tests(self):
         pass
 
-    def getPosCases(self):
+    def get_pos_cases(self):
         pass
 
 class Duke(College):
@@ -34,22 +34,22 @@ class Duke(College):
                                      {"class": "wp-block-table cumulative"})[2].tbody
 
 
-    def querySite(self):
+    def query_site(self):
         res = {}
-        self.page = self.getPage(self.site)
-        numTests = self.getNumberTests()
+        self.page = self.get_page(self.site)
+        numTests = self.get_number_tests()
         res['total_tests'] = numTests
-        posCases = self.getPosCases()
+        posCases = self.get_pos_cases()
         res['total_pos_cases'] = posCases
         return res
 
-    def getNumberTests(self):
+    def get_number_tests(self):
         data = self.totalTable.tr.td.nextSibling
         num = data.text
         total = int(num.replace(',',''))
         return total
 
-    def getPosCases(self):
+    def get_pos_cases(self):
         data = self.totalTable.tr.td.nextSibling.nextSibling
         num = data.text
         total = int(num.replace(',',''))
@@ -75,17 +75,60 @@ class UIUC(College):
         return testsNum
         await browser.close()
 
-    def querySite(self):
+    def query_site(self):
         pass
 
-    def getPage(self, site: str):
+    def get_page(self, site: str):
         asyncio.get_event_loop().run_until_complete(self.go())
 
-    def getNumberTests(self):
+    def get_number_tests(self):
         return self.testNum
 
-    def getNumberTests(self):
+    def get_number_tests(self):
         pass
 
-college = UIUC()
-college.querySite()
+class Harvard(College):
+    def __init__(self):
+        College.__init__(self, 'Harvard',
+                         'https://www.harvard.edu/coronavirus/harvard-university-wide-covid-19-testing-dashboard' ,'Cambridge, Massachussetts')
+        self.list = self.page.select('div.card__text h1')
+
+
+    def query_site(self):
+        res = {}
+        tests = self.get_number_tests()
+        posCases = self.get_pos_cases()
+        posRate = self.getPosRate()
+        iso = self.get_iso()
+        res['total_tests'] = tests
+        res['pos_cases'] = posCases
+        res['pos_rate'] = posRate
+        res['isolation'] = iso
+        print(res)
+
+    def get_pos_cases(self):
+        num = self.list[9].text
+        return int(num.replace(',',''))
+
+    def get_number_tests(self):
+        num = self.list[13].text
+        total = int(num.replace(',',''))
+        return total
+
+    def getPosRate(self):
+        num = self.list[2].text
+        perc = float(num[0:-2])
+        return perc
+
+    def get_iso(self):
+        num = self.list[3].text
+        iso = int(num[0:-1])
+        return iso
+
+college = Duke()
+res = college.query_site()
+print(res)
+
+college = Harvard()
+res = college.query_site()
+print(res)

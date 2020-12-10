42db2474f87636f58c01450aa784375c6c635df5
joshu <joshuapetitma@yahoo.com>
2020-12-10 06:24:22 -0500

    makes school page header typography element

--- src/SchoolPageComponents/SchoolPage.js
+++ src/SchoolPageComponents/SchoolPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./SchoolPage.css";
+import { Typography } from "@material-ui/core/";
 import PolicySection from "./PolicySection";
 import StatisticSection from "./StatisticSection";
 
@@ -7,7 +8,7 @@ import StatisticSection from "./StatisticSection";
 function SchoolPage(props) {
     return (
         <div id="school-page">
-            <h1 className="school-page-header">{props.name} Covid Tracker</h1>
+            <Typography component="h1" align="center" variant="h4" className="school-page-header"><b>{props.name} Covid Tracker</b></Typography>
             <StatisticSection name={props.name} stats={props.stats}/>
             <PolicySection policies={props.policies}/>
         </div>

4c103ef4548cca266fa15c42c2505b5f8a169192
joshu <joshuapetitma@yahoo.com>
2020-12-10 07:46:18 -0500

    adds theme to site

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
 import React from "react";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { red, grey } from "@material-ui/core/colors/";
 import NavBar from "./HomePageComponents/NavBar";
 
+export const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Arial',
+      'Nunito',
+      'Roboto',
+      '"Helvetica Neue"',
+      'sans-serif'
+    ].join(','),
+    h4: {
+      fontSize: "2.5rem",
+    },
+    body1: {
+      fontSize: "2rem",
+      color: "#92a6f8",
+    },
+    button: {
+      fontSize: "1.5rem",
+    },
+  },
+  palette: {
+    secondary: {
+      main: "#92a6f8",
+    },
+  },
+});
+
 export default function App() {
-  return <NavBar />;
+  return (
+  <ThemeProvider theme={theme}>
+    <NavBar />
+  </ThemeProvider>
+    )
+
 }

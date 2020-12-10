97048f27d5a745b5c12023945ef9b954d5705fe6
joshu <joshuapetitma@yahoo.com>
2020-12-10 06:31:28 -0500

    consolidates statistics section

--- src/SchoolPageComponents/SchoolPage.js
+++ src/SchoolPageComponents/SchoolPage.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./SchoolPage.css";
 import { Typography } from "@material-ui/core/";
 import PolicySection from "./PolicySection";
-import StatisticSection from "./StatisticSection";
 
 function SchoolPage(props) {
   return (
@@ -15,8 +14,41 @@ function SchoolPage(props) {
       >
         <b>{props.name} Covid Tracker</b>
       </Typography>
-      <StatisticSection name={props.name} stats={props.stats} />
+
+      <div id="statistic-section">
+        <h2>{props.name + " by the numbers"}</h2>
+        <div id="statistic-elements">
+          <Statistic
+            statistic={{ name: "Total cases", stat: props.stats.pos_cases }}
+          />
+          <Statistic
+            statistic={{ name: "Total tests", stat: props.stats.total_tests }}
+          />
+          <Statistic
+            statistic={{
+              name: "7 Day Positivity Rate",
+              stat: props.stats.pos_rate,
+            }}
+          />
+          <Statistic
+            statistic={{
+              name: "People in Isolation",
+              stat: props.stats.isolation,
+            }}
+          />
+        </div>
+      </div>
+
       <PolicySection policies={props.policies} />
+    </div>
+  );
+}
+
+function Statistic(props) {
+  return (
+    <div id="statistic">
+      <p>{props.statistic.name}</p>
+      <p>{props.statistic.stat == null ? "Unknown" : props.statistic.stat}</p>
     </div>
   );
 }

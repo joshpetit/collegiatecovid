33957390ed606f65192808a3af73750c089b9456
joshu <joshuapetitma@yahoo.com>
2020-12-09 19:54:33 -0500

    reorganizes Landing/home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,30 +1,36 @@
 import React from "react";
+import { Grid, Divider, Card, Typography, CardContent } from "@material-ui/core/";
 import "./Landing.css"
 
+const style = {
+    height: '100vh',
+    background: 'cornflowerblue',
+}
+
+const cardStyle = {
+    minHeight: '50%',
+    alignItems: 'center',
+    minWidth: '100%',
+}
 
 export default function HomePage(){
     return (
-        <div>
-
-                <div className="Splash-container">
-                    <div className="Splash">
-                        <div className="Splash-head">
-                            This is your Universal Covid-Tracker
-                        </div>
-                        <div className="splash-subhead" style={{color: 'white' }}>
-                            <div style={{color: 'white' }}>
-                                Easily find SARS-CoV-2 data from universities across the US
-                            </div>
-                            <div>
-                                Learn about SARS-CoV-2 mitigation policies
-                            </div>
-                            <div>
-                                Compare different university responses
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-        </div>
+        <Grid container direction="column" align="center" alignItems="center" style={style}  justify="center" >
+            <Grid item xs={10} >
+                <Card raised={true} style={cardStyle}>
+                    <CardContent>
+                        <Typography component='h1' variant='h3' align='center'>
+                            Collegiate Covid
+                        </Typography>
+                        <Divider />
+                        <Typography style={{marginTop: '10px', fontSize: '3vh'}} variant='subtitle2' align='center'>
+                            Easily find SARS-CoV-2 data from universities across the US
+                            Learn about SARS-CoV-2 mitigation policies
+                            Compare different university responses
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
     )
 }

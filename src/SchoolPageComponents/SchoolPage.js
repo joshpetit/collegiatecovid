c9daef2c686b66e7478816121421eb7428157aec
joshu <joshuapetitma@yahoo.com>
2020-12-10 07:08:02 -0500

    creates cards for statistics

--- src/SchoolPageComponents/SchoolPage.js
+++ src/SchoolPageComponents/SchoolPage.js
@@ -1,9 +1,34 @@
 import React from "react";
 import "./SchoolPage.css";
-import { Typography } from "@material-ui/core/";
+import {
+  Box,
+  Grid,
+  Typography,
+  CardHeader,
+  Card,
+  CardContent,
+} from "@material-ui/core/";
 import PolicySection from "./PolicySection";
 
 function SchoolPage(props) {
+  let stats = [
+    {
+      title: "Total Cases",
+      stat: props.stats.pos_cases,
+    },
+    {
+      title: "Total Tests",
+      stat: props.stats.total_Tests,
+    },
+    {
+      title: "7 Day Positivity Rate",
+      stat: props.stats.pos_rate,
+    },
+    {
+      title: "Total in Isolation",
+      stat: props.stats.isolation,
+    },
+  ];
   return (
     <div id="school-page">
       <Typography
@@ -15,42 +40,32 @@ function SchoolPage(props) {
         <b>{props.name} Covid Tracker</b>
       </Typography>
 
-      <div id="statistic-section">
-        <h2>{props.name + " by the numbers"}</h2>
-        <div id="statistic-elements">
-          <Statistic
-            statistic={{ name: "Total cases", stat: props.stats.pos_cases }}
-          />
-          <Statistic
-            statistic={{ name: "Total tests", stat: props.stats.total_tests }}
-          />
-          <Statistic
-            statistic={{
-              name: "7 Day Positivity Rate",
-              stat: props.stats.pos_rate,
-            }}
-          />
-          <Statistic
-            statistic={{
-              name: "People in Isolation",
-              stat: props.stats.isolation,
-            }}
-          />
-        </div>
-      </div>
+      <h2>{props.name + " by the numbers"}</h2>
+      <Grid container align="center" justify="center">
+        {stats.map((x, key) => (
+          <Grid item xs={6} spacing={10}>
+            <Box mr={5} ml={5} mt={3}>
+              <Card style={cardStyle} raised={true}>
+                <CardHeader title={x.title} component="h1" />
+                <CardContent>
+                  <Typography align="center" color="secondary">
+                    {x.stat == null ? "Unknown" : x.stat}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
 
       <PolicySection policies={props.policies} />
     </div>
   );
 }
 
-function Statistic(props) {
-  return (
-    <div id="statistic">
-      <p>{props.statistic.name}</p>
-      <p>{props.statistic.stat == null ? "Unknown" : props.statistic.stat}</p>
-    </div>
-  );
-}
+const cardStyle = {
+  height: "100%",
+  width: "40%",
+};
 
 export default SchoolPage;
